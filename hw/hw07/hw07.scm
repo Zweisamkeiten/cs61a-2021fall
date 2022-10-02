f4fe564641b279d79f081d572c2662a4f6bcd926
Einsam <snitxmhm@gmail.com>
2022-10-02 20:01:53 +0800

    hw07: finished

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,11 +1,25 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
 
-(define (ordered? s) 'YOUR-CODE-HERE)
+(define (ordered? s) 
+  (if (null? (cdr s)) #t
+    (if (> (car s) (car (cdr s)))
+      #f
+      (ordered? (cdr s))
+    )
+  )
+)
 
 (define (square x) (* x x))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+  (if (= exp 1) base
+    (if (even? exp)
+      (square (pow base (quotient exp 2)))
+      (* base (pow base (- exp 1)))
+    )
+  )
+)
